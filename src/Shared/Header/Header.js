7cdf2c8727f7065157f1bc875db3d56845018422
arburhan <buthan190629@outlook.com>
2022-06-25 14:34:46 +0600

    install react router

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../images/logo.png';
 import cart from '../../images/icons/Path 1.png';
 const Header = () => {
+    const x = 0;
     return (
         <nav>
             <div>
@@ -11,7 +12,6 @@ const Header = () => {
                 <a href=""><img src={cart} alt="" /></a>
                 <a href="">login</a>
                 <a href="">sign up</a>
-
             </div>
         </nav>
     );
